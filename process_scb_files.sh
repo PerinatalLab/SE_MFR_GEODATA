8e92683bdfe5f2ae94ff9a06e301a4651570a436
jjuod <j.juodakis@gmail.com>
2017-02-15 15:00:20 +0100

    added safeguards against empty fields

--- process_scb_files.sh
+++ process_scb_files.sh
@@ -5,6 +5,12 @@ of=$wd/education_all.txt
 echo "ID BYEAR CIVSTAND LKF EDU YEAR" > $of
 for y in {1998..2013}
 do
-	awk -v y=${y} -F"\t" 'FNR>1{print $1, $2, $7, $8, $10, y}' \
+	echo "processing year ${y}"
+	awk -v y=${y} -F"\t" 'FNR>1{
+		print $1=="" ? "NA" : $1,
+		$2=="" ? "NA" : $2,
+		$7=="" ? "NA" : $7,
+		$8=="" ? "NA" : $8,
+		$10=="" ? "NA" : $10, y}' \
 		/home/sarah/Desktop/POSTAL/PostalCodes_2015_SCB/BoJ_lev_${y}.txt >> $of
 done
